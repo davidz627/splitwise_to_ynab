e2b8a7f2535705cb4aa9450d712489a6060337fd
David Zhu <davidzhu627@gmail.com>
2022-01-17 11:57:51 -0800

    the most jank MVP ever. It works :shrug:

---
+++ main.py
@@ -0,0 +1,64 @@
+# Get transactions from splitwise
+from splitwise import Splitwise
+import logging
+import configparser
+from ynab_client import YNABClient
+from datetime import datetime
+
+myConfigFilePath = "secrets/credentials.ini"
+
+def parseConfig(configFilePath):
+    config = configparser.ConfigParser()
+    config.read(configFilePath)
+    return config['DEFAULT']
+    
+
+logging.basicConfig(level=logging.DEBUG)
+
+myConfig = parseConfig(myConfigFilePath)
+ynabToken = myConfig["ynabToken"]
+ynabAccountID = myConfig["ynabAccountID"]
+splitwiseGroupId = myConfig["splitwiseGroupId"]
+splitwise_user_id = int(myConfig["splitwiseUserId"])
+splitwise_api_key = myConfig["splitwise_api_key"]
+splitwise_consumer_key = myConfig["splitwise_consumer_key"]
+splitwise_consumer_secret = myConfig["splitwise_consumer_secret"]
+
+
+def getSplitwiseExpenses(from_date, splitwise_user_id):
+    s = Splitwise(splitwise_consumer_key,splitwise_consumer_secret,api_key=splitwise_api_key)
+
+    current = s.getCurrentUser()
+    expenses = s.getExpenses(group_id = splitwiseGroupId, dated_after=from_date)
+    parsedExpenses = []
+    for expense in expenses:
+        expenseUsers = expense.getUsers()
+        for expenseUser in expenseUsers:
+            # Negative net balance is owed to Amber, positive owed to me
+            if splitwise_user_id == expenseUser.getId():
+                parsedExpenses.append(
+                    {
+                        "payee_name": expense.getDescription(),
+                        "date": expense.getDate(),
+                        "amount": int(float(expenseUser.getNetBalance())*1000),
+                        
+                    }
+                )
+    return parsedExpenses
+
+# Change the format of it
+
+# Upload to YNAB
+myYNAB = YNABClient(ynabToken)
+
+def getMostRecentYNABTransactionDate(myYNAB):
+    transactions = myYNAB.listTransactionsInAccount(ynabAccountID)
+    mostRecentTransaction = max(transactions, key = lambda transaction: datetime.strptime(transaction["date"], "%Y-%m-%d").date())
+    return mostRecentTransaction["date"]
+
+mostRecentTransactionDate = getMostRecentYNABTransactionDate(myYNAB)
+expenses = getSplitwiseExpenses(from_date = mostRecentTransactionDate, splitwise_user_id = splitwise_user_id)
+for expense in expenses:
+    expense["account_id"] = ynabAccountID
+    expense["cleared"] = "cleared"
+myYNAB.postTransactions(expenses)
---
+++ ynab_client.py
@@ -0,0 +1,52 @@
+import requests
+import json
+import re
+
+class YNABClient(object):
+    BASE_URL = "https://api.youneedabudget.com/v1"
+
+    def __init__(self, token):
+        self.token = token
+        self.budgetID = self.getBudgetID()
+
+    # TODO: Only gets the 0th budget available.
+    # We should eventually check all budgets just in case
+    def getBudgetID(self):
+        url = self.BASE_URL + "/budgets"
+        headers = {
+            "Authorization": f"Bearer {self.token}",
+            "accept": "application/json",
+        }
+        rawResponse = requests.get(url ,headers=headers)
+        resp = json.loads(rawResponse.content.decode('utf-8'))
+        return resp["data"]["budgets"][0]["id"]
+
+    def postTransactions(self, transactions):
+        if len(transactions) == 0:
+            print("No transactions to patch, skipping...")
+            return
+        url = self.BASE_URL + f"/budgets/{self.budgetID}/transactions"
+        headers = {
+            "Authorization": f"Bearer {self.token}",
+            "accept": "application/json",
+            "Content-Type": "application/json"
+        }
+        data = json.dumps({"transactions": transactions})
+        resp = requests.post(url, data, headers=headers)
+        if resp.status_code != 200:
+            print (f"Something went wrong, got response: {resp.content}")
+        else:
+            print (f"Successfully updated transactions {transactions}")
+
+    def listTransactionsInAccount(self, accountId):
+        since_date = "2022-01-01"
+        url = self.BASE_URL + f"/budgets/{self.budgetID}/accounts/{accountId}/transactions?since_date={since_date}"
+        headers = {
+            "Authorization": f"Bearer {self.token}",
+            "accept": "application/json",
+        }
+        rawResponse = requests.get(url ,headers=headers)
+        resp = json.loads(rawResponse.content.decode('utf-8'))
+        if rawResponse.status_code != 200:
+            return None
+        return resp["data"]["transactions"]
